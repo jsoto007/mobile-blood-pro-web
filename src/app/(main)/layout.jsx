9a9de057c81428e19595c7137444cecc27c6580c
jsoto007 <jsoto7087@gmail.com>
2025-06-29 22:58:27 -0400

    working with no errors

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,1 +1,2 @@
-export { Layout as default } from '@/components/Layout'
+export { /* @next-codemod-error `Layout` export is re-exported. Check if this component uses `params` or `searchParams`*/
+Layout as default } from '@/components/Layout'
